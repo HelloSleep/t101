33ace7e9ab99c3ebfacfc9ad268eb57ee1cf4793
minchan <nargene@gmail.com>
2023-07-29 22:10:45 +0900

    week4 challenges module terraform, github

--- 4week/t101-4week-challenges/module-set/use/main.tf
+++ 4week/t101-4week-challenges/module-set/use/main.tf
@@ -1,6 +1,6 @@
 module security-group{
     source = "../moduels"
-    vpc_id      = "vpc-0cea1bd4e3203a40c"
+    vpc_id      = "<yourvpc_id>"
     name       = "your_security_group_name"
     allow_ssh  = true  # Or set it to "false" if you don't want to allow SSH.
     allow_http = true  # Or set it to "false" if you don't want to allow HTTP.
---
+++ 4week/t101-4week-challenges/terraform_registry/main.tf
@@ -0,0 +1,5 @@
+module "security-group" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "5.1.0"
+  
+}
