04856d43015079626696122d7d8f879927670eea
minchan <nargene@gmail.com>
2023-07-29 23:45:42 +0900

    change directories

--- 4week/t101-4week-challenges/github-module/main.tf
+++ 4week/t101-4week-challenges/github-module/main.tf
@@ -1,6 +1,6 @@
 module "security-group" {
   source = "github.com/HelloSleep/t101/4week/t101-4week-challenges//terraform-aws-security-group"
-    vpc_id      = "vpc-0cea1bd4e3203a40c"
+    vpc_id      = "<your_vpc>"
     name       = "your_security_group_name"
     allow_ssh  = true  # Or set it to "false" if you don't want to allow SSH.
     allow_http = true  # Or set it to "false" if you don't want to allow HTTP.
